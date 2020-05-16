e3a59fd4f616b4f2f231de8984c4c998ae1edf0f
Alex Jimenez <artesalexj@gmail.com>
2020-05-17 01:11:41 +0200

    kata2

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,17 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
+    cifrado = string.ascii_letters + string.punctuation +string.hexdigits
+    miclave=''.join(random.choice(cifrado) for letra in range(passLen))
+    return miclave
     
-    #
-    #
+clave=RandomPasswordGenerator(passLen=10)
+print(clave)
 
-    return ""
+def ClaveAleatoria(tamano):
+    cifrado=string.ascii_letters + string.digits + string.punctuation
+    miclave = ''.join(random.choice(cifrado) for letra in range(tamano))
+    return miclave
+
+contraseña = ClaveAleatoria(10)
+print(contraseña)
