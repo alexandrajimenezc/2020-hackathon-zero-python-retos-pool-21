26f5dc0c6c9814a80ef95672e8160de3ac1fa065
alexandrajimenezc <artesalexj@gmail.com>
2020-05-17 17:54:15 +0200

    kata3

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,36 +12,49 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    return update.message.reply_text('Hola , soy creado por javi y Alex!')
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    return update.message.reply_txt('Ayudame!')
 
 def mayus(update, context):
-        #
-        return ""
+    """Renvia el msj en mayuscula /mayus."""
+    return update.message.reply_text(update.message.text.upper())
 
 def alreves(update, context):
-    """Repite el mensaje del usuario."""
-    #
-    return ""
+    """Repite el mensaje del usuario y lo devuleve al reves."""
+    return update.message.reply_text(update.message.text[::-1])
+
+def repite(update, context):
+    """Repite el mensaje del usuario /echo"""
+    update.message.reply_text(update.message.text) 
+
+def comandos(update, context):
+    """Te dice los comandos disponibles"""
+    update.message.reply_text('Hola , estos son los comando disponibles:\n/start : inicia el bot\n/help\n/mayus : convierte el texto en mayúscular \n/alreves : Te devuelve el texto alreves\n /repite : Repite lo que escribes ')
 
 def error(update, context):
     """Envia los errores por consola"""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
+
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1100105888:AAHwcgDxQ7l7jwC2if1mNzeTFHCbweQQxx", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help",help))
+    dp.add_handler(CommandHandler("mayus",mayus))
+    dp.add_handler(CommandHandler("alreves",alreves))
+    dp.add_handler(CommandHandler("repite",repite))
+    dp.add_handler(CommandHandler("comandos",comandos))
+
     #
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
